ce99fa034124b51162b84f144ccbc31756c51385
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2015-07-03 20:54:17 +0200

    Beautify

--- src/__tests__/interactionsTests.js
+++ src/__tests__/interactionsTests.js
@@ -4,28 +4,28 @@ jest.dontMock('../interactions.js').dontMock('../parser.js').dontMock('lodash');
 
 var interactions = require('../interactions.js');
 var parser = require('../parser.js');
-var _=require('lodash');
+var _ = require('lodash');
 
 describe('interactions', function() {
-  var interactions = require('../interactions');
 
   describe('list of interactions', function() {
 
     it('simple', function() {
       expect(interactions.listOfInteractions(
-        parser.parse("(a)", {startRule: "interaction"}))).toEqual(
+        parser.parse("(a)", {
+          startRule: "interaction"
+        }))).toEqual(
         ["a"]
       );
     });
 
     it('composite', function() {
-      expect(_.sortBy
-        (interactions.listOfInteractions(
-        parser.parse("({a:(x),b:((y)+(5))})", {startRule: "interaction"})))
-      ).toEqual(
-        _.sortBy
-        (["x","y","5","$+$","{a:$,b:$}"])
+      expect(_.sortBy(interactions.listOfInteractions(
+        parser.parse("({a:(x),b:((y)+(5))})", {
+          startRule: "interaction"
+        })))).toEqual(
+        _.sortBy(["x", "y", "5", "$+$", "{a:$,b:$}"])
       );
     });
-});
+  });
 });
