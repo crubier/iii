662e5ca164063db7dc0e6159a899e1e887524706
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2015-07-20 14:00:31 +0200

    Add export of interactions

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var parser = require('./dist/parser.js');
 
 var interfaces = require('./dist/interfaces.js');
 
-
-
+var interactions = require('./dist/interactions.js');
 
 module.exports.parser = parser;
 
 module.exports.interfaces = interfaces;
+
+module.exports.interactions = interactions;
