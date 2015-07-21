fb5ad2669e40e66c9cb3db11d6422d850b933922
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2015-07-21 23:02:28 +0200

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,37 @@ var del   = require('del');
 
 var jest = require('gulp-jest');
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+gulp.task('browserify', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './index.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('iii-min.js'))
+    .pipe(buffer())
+    // .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    // .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+
+
+
+
+
 gulp.task('jest',['dist'], function () {
     return gulp.src('./').pipe(jest({
         // scriptPreprocessor: "./spec/support/preprocessor.js",
